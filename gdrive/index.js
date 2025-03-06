3c6856835c07d4779ddf0d960303e635c8a13385
hetp4401 <patelhet32@gmail.com>
2025-03-05 17:13:45 -0800

    extract cf ip

--- gdrive/index.js
+++ gdrive/index.js
@@ -48,7 +48,7 @@ router.get("/stream/:type/:tt.json", async (req, res) => {
 
 router.get("/unrestrict/:b64url", async (req, res) => {
   res.redirect(302, await unrestrictTorrentio(req.params.b64url));
-  saveActivity(req.params.b64url, req.ip);
+  saveActivity(req.params.b64url, req.headers["cf-connecting-ip"] || req.ip);
 });
 
 router.get("/activity", async (req, res) => {
